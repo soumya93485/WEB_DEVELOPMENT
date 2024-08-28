b924aabf5dc4d3fd4ad724fbad03c4dc4fd83736
soumya93485 <soumyaranjann7894@gmail.com>
2024-08-28 21:05:22 +0530

    update

---
+++ Javascript/class-3/task/index.js
@@ -0,0 +1,27 @@
+// Addition
+
+// let a = 10;
+// let b = 20;
+// let c= a+b;
+// console.log(c); // 30
+
+//Subtraction
+
+// let a = 20;
+// let b = 10;
+// let c= a-b;
+// console.log(c); // 10
+
+//Multiplication 
+
+// let a = 6;
+// let b = 5;
+// let c= a*b;
+// console.log(c); // 30
+
+// Divison
+
+let a = 8;
+let b = 6;
+let c= a/b;
+console.log(c); // 1.3333333333333333
